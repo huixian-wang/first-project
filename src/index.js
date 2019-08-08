821cb22cf86925dd99984a8042a91db64ec3cdae
wanghuixian <wanghuixian@sunlands.com>
2019-08-08 10:44:22 +0800

    update components

--- src/index.js
+++ src/index.js
@@ -1,71 +1,21 @@
-// require('./style.css')
-// // require('./images/test.jpg')
-// document.onload = _ => {
-//     const app = document.getElementById('app')
-//     // app.
-//     console.log(app);
-// }
-// console.log('enter')
-// console.log('hahhahahhaha')
-// console.log('heieheiheiei')
-
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { createLogger } from 'redux-logger';
-// import { createLogger } from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Route, hashHistory, Provider, browserHistory } from 'react-redux';
 import { routerReducer } from 'react-router-redux'
-import Page1 from './pages/Page'
+
 import reducers from './reducer/index'
 import AppRouter from './router'
 import action from './action/page.js'
-// import todo from './components/Todo.js'
-// export default class App extends Component {
-//     render() {
-//         return (
-//             <div>Hello React!</div>
-//         )
-//     }
-// }
 
 const store = createStore(
     reducers,
     applyMiddleware(createLogger()),
-    // combineReducers({ reducers, routerReducer }),
-    // { test: "" },
-    // enhancer,
 )
-// console.log(store, 'store')
-// 封装dispatch --test
-// let next = store.dispatch
-// store.dispatch = action => {
-//     console.log(action)
-//     next(action)
-//     console.log(store.getState())
-// }
-
-
-// let unsubscribe = store.subscribe(() =>
-//   console.log(store.getState(), 9999999)
-// )
-// unsubscribe()
-// store.subscribe(() =>
-//     console.log(store.getState(), 8888888)
-// )
-// store.subscribe(() =>
-//     console.log(store.getState(), 777777)
-// )
-
-
-// store.dispatch(action.changeText('test for redux-action'))
-// store.dispatch(action.changeText('test for redux-action will stop'))
-// console.log(store.getState(), 99999999)
 render(
     <Provider store={store}>
         <AppRouter history={browserHistory} />
     </Provider>,
-    // <Page1 />,
-    // <AppRouter history={browserHistory}/>,
     document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,12 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // mock配置
 var apiMocker = require('webpack-api-mocker')
-// var mocker = require('./src/mock')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname + '/dist',
-        filename: 'index-[chunkhash].js'
-        // publicPath: 'dist/'
+        filename: 'index-[hash].js'
     },
     devtool: 'source-map',
     devServer: {
@@ -23,87 +21,40 @@ module.exports = {
         inline: true,
         open: true,
         port: 8111,
-        // before(app){
-        //     apiMocker(app, path.resolve('./src/mock.js'), {
-        //         proxy: {
-        //             '/texstdata//*': 'https://api.github.com/',
-        //         },
-        //         changeHost: true,
-        //     })
-        // }
         proxy: {
             '/profile': {
                 target: 'http://localhost:3000',
-                // pathRewrite: {^/}
                 changeOrigin: true,
-                // pathRewrite: function(path, req) {
-                //     return path.replace(/^\/testdata/, '/testdata')
-                // },
-                // onProxyReq: function(proxyReq, req, res) {
-                //     proxyReq.method = 'GET';
-                //     proxyReq.setHeader('Access-Control-Allow-Origin', true);
-                // },
-                // bypass: function(req, res, proxyOptions) {
-                //     var noProxy = [
-                //         // '/api/course/courseList.action'
-                //     ];
-                //     if (noProxy.indexOf(req.url) !== -1) {
-                //         console.log('Skipping proxy for browser request.');
-                //         return req.url;
-                //     }
-                // },
             }
-        }
+        },
+        hot: true,
     },
     module: {
         rules: [{
             test: /\.css$/,
-            // use: ExtractTextPlugin.extract({
-            //     // filename: '/dist/style.css',
-            //     // use: [{
-            //         loader: 'css-loader',
-            //         // option: {
-            //         //     importLoaders: 2
-            //         // }
-            //     // }]
-            // })
             use: ExtractTextPlugin.extract(['css'])
         },{
             test: /\.less$/,
-            // use:[ 'style-loader','css-loader','less-loader']
             use: [{
                 loader: "style-loader"
             },{
                 loader: "css-loader?modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]",
-                // options: {
-                //     modules: true,
-                //     localIdentName: '[name]__[local]--[hash:base54:5]'
-                // }
             },{
                 loader: "less-loader"
             }]
         },{
             test: /\.(jpg|png|gif)$/,
-            // loader: 'url-loader?limit=8192'
             loader: 'url-loader?limit=8192&name=images/[name].[ext]'
-            // use: [{
-            //     loader: 'url-loader',
-            //     options: {
-            //         limit: 8192
-            //     }，
         },{
             test: /\.(js|jsx)$/,
-            // loader: 'babel-loader?preset=[[env], [react]]'
             use: {
                 loader: 'babel-loader'
-                // options: {
-                //     preset: ['env', 'react']
-                // }
             },
             exclude: /node_modules/
         }]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',

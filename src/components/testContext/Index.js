9306fee10d10a132f371c729df6dc33ae5f1265b
wanghuixian <wanghuixian@sunlands.com>
2020-01-17 11:06:37 +0800

    update

---
+++ src/components/testContext/Index.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+
+class Com extends React.Component{
+    constructor(props, context){
+        super(props)
+    }
+    render (){
+        return (
+            <div>
+                <p>test context1</p>
+            </div>
+        )
+    }
+}
+
+export default Com
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Router, Route, IndexRoute, Redirect, hashHistory, browserHistory, Lifecycle } from 'react-router'
 import Home from './components/Home'
+import TestContext from './components/testContext/Index.js'
 
 class AppRouter extends Component {
     componentDidMount() {
@@ -10,8 +11,8 @@ class AppRouter extends Component {
     render() {
         return (
              <Router history={hashHistory}>
-                <Route path='/' component={Home}>
-                </Route>
+                <Route path='/' component={Home} />
+                <Route path='/context' component={TestContext} />
             </Router>
         )
     }
